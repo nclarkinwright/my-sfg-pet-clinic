b9a464e3f47004c7671271f9fe0c516ac5016607
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2021-09-02 12:59:09 -0400

    Initial project creation with Spring Initializr

---
+++ src/main/java/nclarkinwright/mysfgpetclinic/MySfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package nclarkinwright.mysfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySfgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySfgPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nclarkinwright/mysfgpetclinic/MySfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package nclarkinwright.mysfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

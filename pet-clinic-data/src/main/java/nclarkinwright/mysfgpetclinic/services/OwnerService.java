62577dfab23eb43de995e9fb49ef470cb39d313e
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2021-09-03 19:47:09 -0400

    Added service interfaces for db

---
+++ pet-clinic-data/src/main/java/nclarkinwright/mysfgpetclinic/services/OwnerService.java
@@ -0,0 +1,12 @@
+package nclarkinwright.mysfgpetclinic.services;
+
+import nclarkinwright.mysfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findById(Long id);
+    Owner findByLastName(String lastName);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/nclarkinwright/mysfgpetclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package nclarkinwright.mysfgpetclinic.services;
+
+import nclarkinwright.mysfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}

f7d1a560343ea9f395f1dd1cd8c0022b70685947
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2021-09-08 14:29:45 -0400

    Refactoring services to common CRUD service

--- pet-clinic-data/src/main/java/nclarkinwright/mysfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/nclarkinwright/mysfgpetclinic/services/PetService.java
@@ -4,8 +4,6 @@ import nclarkinwright.mysfgpetclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
+
 }

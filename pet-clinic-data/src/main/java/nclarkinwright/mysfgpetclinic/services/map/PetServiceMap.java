9f9677aa2efbce98f5766ea680ee8530ff2fd98b
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2021-09-13 13:32:38 -0400

    Refactor for *ServiceMap classes

--- pet-clinic-data/src/main/java/nclarkinwright/mysfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/nclarkinwright/mysfgpetclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package nclarkinwright.mysfgpetclinic.services.map;
 
 import nclarkinwright.mysfgpetclinic.model.Pet;
-import nclarkinwright.mysfgpetclinic.services.CrudService;
+import nclarkinwright.mysfgpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/nclarkinwright/mysfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/nclarkinwright/mysfgpetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package nclarkinwright.mysfgpetclinic.services.map;
 
 import nclarkinwright.mysfgpetclinic.model.Vet;
-import nclarkinwright.mysfgpetclinic.services.CrudService;
+import nclarkinwright.mysfgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();

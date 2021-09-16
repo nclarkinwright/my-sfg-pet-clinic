bad0ef0bc295487bb6fdb1ec874519e1950a743b
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2021-09-16 14:04:52 -0400

    Added vet speciality

---
+++ pet-clinic-data/src/main/java/nclarkinwright/mysfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package nclarkinwright.mysfgpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

9695addcbc2647bb66a422a470f5f1f5a9059ece
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2021-09-16 14:26:27 -0400

    Added sample PetTypes to bootstrap data

--- pet-clinic-web/src/main/java/nclarkinwright/mysfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/nclarkinwright/mysfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package nclarkinwright.mysfgpetclinic.bootstrap;
 
 import nclarkinwright.mysfgpetclinic.model.Owner;
+import nclarkinwright.mysfgpetclinic.model.PetType;
 import nclarkinwright.mysfgpetclinic.model.Vet;
 import nclarkinwright.mysfgpetclinic.services.OwnerService;
+import nclarkinwright.mysfgpetclinic.services.PetTypeService;
 import nclarkinwright.mysfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType saveCatType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");

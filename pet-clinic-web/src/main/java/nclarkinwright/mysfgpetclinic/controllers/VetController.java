71009b5aba72c9b87c2dad5099c363bb5d88f3c1
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2021-09-17 15:44:29 -0400

    Fixed broken links on home page with placeholder

--- pet-clinic-web/src/main/java/nclarkinwright/mysfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/nclarkinwright/mysfgpetclinic/controllers/VetController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class VetController {
 
@@ -14,7 +15,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets", "vets.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";

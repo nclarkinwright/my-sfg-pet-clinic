968f6502c03e89b0123950e93a58d147e326f41e
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2021-09-17 14:28:19 -0400

    Added SpecialityMapService and SpecialityService

---
+++ pet-clinic-data/src/main/java/nclarkinwright/mysfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package nclarkinwright.mysfgpetclinic.services;
+
+import nclarkinwright.mysfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}

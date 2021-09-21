17536f888fbf22e99522620ac364f88f14d14b0b
Nicholas Clarkin-Wright <nicholas.clarkin-wright@outlook.com>
2021-09-21 10:26:07 -0400

    Added missing service annotation

--- pet-clinic-data/src/main/java/nclarkinwright/mysfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/nclarkinwright/mysfgpetclinic/services/map/VisitMapService.java
@@ -2,9 +2,11 @@ package nclarkinwright.mysfgpetclinic.services.map;
 
 import nclarkinwright.mysfgpetclinic.model.Visit;
 import nclarkinwright.mysfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
     @Override
